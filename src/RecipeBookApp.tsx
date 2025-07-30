3e1a38680244a4bb6aab811265dbe6f9fd05ad45
Manny <manny.blum@gmail.com>
2025-07-30 12:28:50 -0400

    updating queryclientprovider

--- src/RecipeBookApp.tsx
+++ src/RecipeBookApp.tsx
@@ -1,10 +1,28 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { createAsyncStoragePersister } from "@tanstack/query-async-storage-persister";
+import { persistQueryClient } from "@tanstack/query-persist-client-core";
+
 import RecipeBook from "./components/RecipeBook";
 import RecipeBookProvider from "./context/RecipeBookContext";
 
+const queryClient = new QueryClient();
+
+const persister = createAsyncStoragePersister({
+  storage: window.localStorage,
+});
+
+persistQueryClient({
+  queryClient,
+  persister,
+  maxAge: 1000 * 60 * 60, // 1 hour
+});
+
 export default function RecipeBookApp() {
   return (
-    <RecipeBookProvider>
-      <RecipeBook />
-    </RecipeBookProvider>
+    <QueryClientProvider client={queryClient}>
+      <RecipeBookProvider>
+        <RecipeBook />
+      </RecipeBookProvider>
+    </QueryClientProvider>
   );
 }
