e0758b14af4920c96cf183dc8dc7b70d3700c1ad
Manny <manny.blum@gmail.com>
2025-07-31 12:06:33 -0400

    tweaking some settings for submodule setup

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 // import react from "@vitejs/plugin-react-swc";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-
+import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
   server: {
@@ -15,4 +15,16 @@ export default defineConfig({
     },
   },
   plugins: [react(), tailwindcss()],
+  build: {
+    outDir: "dist",
+    lib: {
+      entry: path.resolve(__dirname, "src/index.tsx"),
+      name: "Recipebook",
+      fileName: () => "recipebook.js",
+      formats: ["es"],
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+    },
+  },
 });
